26e6f527be4f8ac7d7618b6854bb53ed6b01976b
ujef malek <ujefmalek49@gmail.com>
2022-06-30 18:38:11 +0530

    second commit

--- chat.py
+++ chat.py
@@ -1,12 +1,19 @@
 import random
 import json
-
 import torch
-
+from flask import Flask,request,jsonify
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+app = Flask(__name__)
+
+@app.route('/res',methods=['GET'])
+def hello():
+   d={}
+   d['que'] = str(request.args['que'])
+   d['ans'] = get_response(d['que'])
+   return jsonify(d)
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
@@ -21,7 +28,7 @@ all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
 
-model = NeuralNet(input_size, hidden_size, output_size).to(device)
+model = NeuralNet(input_size, hidden_size, output_size)
 model.load_state_dict(model_state)
 model.eval()
 
@@ -31,7 +38,7 @@ def get_response(msg):
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
-    X = torch.from_numpy(X).to(device)
+    X = torch.from_numpy(X)
 
     output = model(X)
     _, predicted = torch.max(output, dim=1)
@@ -49,13 +56,5 @@ def get_response(msg):
 
 
 if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence)
-        print(resp)
+    app.run()
 
